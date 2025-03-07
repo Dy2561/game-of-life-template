b76815a3d125a1b6007c02daecdb8bc178faff57
Dy2561 <dylanyun2009@gmail.com>
2025-03-07 07:20:51 +0000

    Change 1

--- Main.java
+++ Main.java
@@ -1,19 +1,16 @@
 public class Main {
+    public static void main(String[] args) {
+        GameOfLife g = new GameOfLife(7, 7);
 
-    public static void main(String[] args)
-    {
-        // GameOfLife g = new GameOfLife(7, 7);
+        int[][] data = {
+            { 0, 1, 0 },
+            { 0, 0, 1 },
+            { 1, 1, 1 }
+        };
 
-        // int[][] data = {
-        //     { 0, 1, 0},
-        //     { 0, 0, 1},
-        //     { 1, 1, 1 }
-        // };
+        g.set(1, 1, data);
+        g.run(5); 
 
-        // g.set(1, 1, data);
-
-        // g.run(5);
-
-        TestSuite.run();
+        TestSuite.run(); 
     }
 }
